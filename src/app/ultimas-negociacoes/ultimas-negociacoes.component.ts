b4d8558df470bf388e2fd70993a848c715eaea94
Guilherme Vargas <gvargas159z@gmail.com>
2023-09-18 15:35:29 -0300

    feat: base inicial

--- src/app/ultimas-negociacoes/ultimas-negociacoes.component.ts
+++ src/app/ultimas-negociacoes/ultimas-negociacoes.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ultimas-negociacoes',
   templateUrl: './ultimas-negociacoes.component.html',
   styleUrls: ['./ultimas-negociacoes.component.css']
 })
-export class UltimasNegociacoesComponent {
+export class UltimasNegociacoesComponent implements OnInit{
+
+  public inputSearch: string = ""
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  searchButton() {
+    
+  }
 
 }
