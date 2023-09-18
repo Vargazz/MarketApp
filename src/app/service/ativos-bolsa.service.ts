3c796a078c8564648593fe37e47dad5e3f3da785
Guilherme Vargas <gvargas159z@gmail.com>
2023-09-18 18:57:59 -0300

    feat: Consumindo API e renderizando as ultimas 5 negociacoes

--- src/app/service/ativos-bolsa.service.ts
+++ src/app/service/ativos-bolsa.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { API_KEY, API_PATH } from 'src/environments/environments';
 
 @Injectable({
@@ -17,7 +18,7 @@ export class AtivosBolsaService {
     
   }
 
-  obterAtivo() {
-    return this.httpCliente.get(`${API_PATH}query?function=TIME_SERIES_DAILY&symbol=${this.ATIVO}&apikey=${API_KEY}`).toPromise();
+  obterAtivo(): Observable<[]> {
+    return this.httpCliente.get<[]>(`${API_PATH}query?function=TIME_SERIES_DAILY&symbol=${this.ATIVO}&apikey=${API_KEY}`);
   }
 }
--- src/app/ultimas-negociacoes/ultimas-negociacoes.component.ts
+++ src/app/ultimas-negociacoes/ultimas-negociacoes.component.ts
@@ -10,21 +10,33 @@ export class UltimasNegociacoesComponent implements OnInit{
 
   public inputSearch: string = ""
 
+  public ativosArray: any
+
+  public ultimas5Datas: string[] = []
+
   constructor(private ativosService: AtivosBolsaService) {
 
   }
 
   ngOnInit(): void {
-    
+
+  } 
+
+  setUltimo() {
+    const timeSeries = this.ativosArray["Time Series (Daily)"]
+    const altSeries = Object.keys(timeSeries).reverse()
+    this.ultimas5Datas = altSeries.slice(-5).reverse()
   }
 
   searchButton() {
     this.ativosService.recebeInput(this.inputSearch)
 
-    this.ativosService.obterAtivo()
-      .then(ativo => console.log(ativo))
-      .catch(error => console.log(error))
-
+    this.ativosService.obterAtivo().subscribe((ativo) => {
+      
+      this.ativosArray = ativo
+      
+      this.setUltimo()
+    })
     this.inputSearch = ""
   }
 
